b91d6ddd86fd5a27e6ec3c66ea47458e6ce68d27
sevla1own <sevla1own@gmail.com>
2022-12-28 22:28:48 -0300

    feat: add fields validation

---
+++ src/shared/infra/http/middlewares/validator.ts
@@ -0,0 +1,40 @@
+import { NextFunction, Request, Response } from "express";
+
+import Joi from "joi";
+
+export type ISchemaType = {
+  body?: Joi.ObjectSchema<unknown>;
+  params?: Joi.ObjectSchema<unknown>;
+  query?: Joi.ObjectSchema<unknown>;
+};
+
+export const Validator = (schema: ISchemaType) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    const errors: string[] = [];
+
+    if (schema.body) {
+      const body = schema.body.validate(req.body);
+      if (body.error) {
+        body.error.details.map((err) => errors.push(err.message));
+      }
+    } else if (schema.params) {
+      const params = schema.params.validate(req.params);
+
+      if (params.error) {
+        params.error.details.map((err) => errors.push(err.message));
+      }
+    } else if (schema.query) {
+      const query = schema.query.validate(req.query);
+
+      if (query.error) {
+        query.error.details.map((err) => errors.push(err.message));
+      }
+    }
+
+    if (errors.length) {
+      return res.status(400).json({ r: false, errors });
+    }
+
+    next();
+  }
+}
